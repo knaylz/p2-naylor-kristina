434157442e9f34cc8d08cf9cc38a48b1c01c3287
Kristina Naylor <kristinamarienaylor@gmail.com>
2020-07-12 08:45:33 -0400

    Add js read less read more button and staff images and flowers to what we do page

---
+++ main.js
@@ -0,0 +1,16 @@
+//Read More Read Less button
+function read() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("read");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
