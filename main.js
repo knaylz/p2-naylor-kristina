a75553650e2fd1657f10a5914ef6c061ec49b871
Kristina Naylor <kristinamarienaylor@gmail.com>
2020-07-24 14:58:37 -0400

    Add API joke generator and tabbed content functionality

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 //Read More Read Less Button
+var readMoreButtons = document.getElementsByClassName('read');
+
 function read() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("read");
+  var dots = this.parentElement.children[0].children[0];
+  var moreText = this.parentElement.children[0].children[1];
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
+    this.innerHTML = "Read more";
     moreText.style.display = "none";
   } else {
     dots.style.display = "none";
-    btnText.innerHTML = "Read less";
+    this.innerHTML = "Read less";
     moreText.style.display = "inline";
   }
+}
+
+for (i = 0; i < readMoreButtons.length; i++) {
+  readMoreButtons[i].addEventListener('click', read);
 }
 
 //Hamburger Menu
@@ -23,45 +28,64 @@ hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
 });
 
-
 //Tabbed Content
 
-function onTabClick(event) {
-  let activeTabs = document.querySelectorAll('.active');
+var navTabs = document.getElementById('nav-tab').children;
 
-activeTabs.forEach(function(tab) {
-  tab.className = tab.className.replace('active', '');
-});
+function onTabClick() {
+  for (i = 0; i < navTabs.length; i++) {
+    navTabs[i].classList.remove('active');
+  }
 
-// activate new tab and panel
-event.target.parentElement.className += ' active';
-document.getElementById(event.target.href.split('#')[1]).className += ' active';
+  var allTabs = document.getElementsByClassName('tab-pane');
+     
+  for (i = 0; i < allTabs.length; i++) {
+     allTabs[i].style.display = 'none';
+    }
+
+  this.classList.add('active');
+
+  var concertsContent = document.getElementById('concerts');
+  var yogaContent = document.getElementById('yoga');
+  var cleanupsContent = document.getElementById('cleanups');
+  var sponsorContent = document.getElementById('sponsor');
+
+  if (this.children[0].innerHTML == 'Concerts') {
+    concertsContent.style.display == 'block';
+  } else if (this.children[0].innerHTML == 'Yoga In The Park') {
+    yogaContent.style.display = 'block';
+  } else if (this.children[0].innerHTML == 'Cleanups') {
+    cleanupsContent.style.display = 'block';
+  } else if (this.children[0].innerHTML == 'Sponsor Message') {
+    sponsorContent.style.display = 'block';
+  } else {
+    return;
+  }
+}  
+
+  for (i = 0; i < navTabs.length; i++) {
+    navTabs[i].addEventListener('click', onTabClick);
+  }
+
+// Random Chuck Norris Jokes API
+
+var button = document.getElementById('generate-joke-button');
+
+function generateRandomJoke() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      var apiResult = JSON.parse(this.responseText);
+
+      // Populate Jokes
+        var chuckNorrisJokeParagraph = document.getElementById('chuck-norris-joke');
+        chuckNorrisJokeParagraph.innerHTML = apiResult.value.joke;
+      }
+};
+xmlhttp.open('GET', 'http://api.icndb.com/jokes/random', true);
+xmlhttp.send();
 }
 
-const element = document.getElementById('nav-tab');
-
-element.addEventListener('click', onTabClick, false);
-
-//Popup Ad
-//window.onload = function () {
-//  document.getElementById('button').onclick = function () {
-//      document.getElementById('modal').style.display = "none"
- // };
-//};       
-
-//Random Quotes
-//var quotes = ["look in the mirror. Are you looking at me?",
-//              "It is time for a rhyme, I guess.",
- //             "Where is my JavScript book?",
- //             "If I had a buck for every dollar I spent. Ooops!"
- //             "I am quote number 5",
-  //            "I am quote number 6",
- //             "I am quote number 7",
- //             "I am quote number 8",
- //             "I am quote number 9",
- //             "I am quote number 10"
-//];
-
-//var q_div = document.getElementById("my-quote");
-////var rand_int = Math.floor(Math.random() * 10);
-//q_div.innerHTML = quotes[rand_int];
+ // Button Click Event Listener
+ button.addEventListener('click', generateRandomJoke);
